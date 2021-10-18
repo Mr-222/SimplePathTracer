60cf9dd6d7a0d0fad0f8fd1ce8e18482f27b7ecf
Mr-222 <821628267@qq.com>
2021-10-18 23:07:35 +0800

    Create project

---
+++ cmake-build-debug/CMakeFiles/my_pathtracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_pathtracer.dir/main.cpp.o"
+  "my_pathtracer"
+  "my_pathtracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_pathtracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
