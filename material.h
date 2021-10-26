dca61d9c93643c18e65cd26820623e9df42459be
Mr-222 <821628267@qq.com>
2021-10-26 18:39:37 +0800

    fix a bug of dielectric material(material.h)

--- material.h
+++ material.h
@@ -59,10 +59,10 @@ public:
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
             ) const override {
         attenuation = color(1.0, 1.0, 1.0);
-        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
 
         vec3 unit_direction = unit_vector(r_in.direction());
-        double cos_theta = fmin(dot(-unit_direction, rec.p), 1.0);
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
         double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
 
         bool cannot_refract = refraction_ratio * sin_theta > 1.0;
