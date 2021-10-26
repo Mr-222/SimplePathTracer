b13326677267c42f292b39db3dc80a67c17b5dd0
Mr-222 <821628267@qq.com>
2021-10-26 23:11:58 +0800

    add depth of field effect

--- camera.h
+++ camera.h
@@ -10,25 +10,35 @@ public:
             point3 lookat,
             vec3 vup,
             double vfov,  // vertical field-of-view in degrees
-            double aspect_ratio
+            double aspect_ratio,
+            double aperture,
+            double focus_dist
             ) {
         double theta = degrees_to_radians(vfov);
         double h = tan(theta / 2);
         double viewport_height = 2.0 * h;
         double viewport_width = aspect_ratio * viewport_height;
 
-        vec3 w = unit_vector(lookfrom - lookat);
-        vec3 u = unit_vector(cross(vup, w));
-        vec3 v = cross(w, u);
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
 
         origin = lookfrom;
-        horizontal = viewport_width * u;
-        vertical = viewport_height * v;
-        lower_left_corner = origin - horizontal/2 - vertical/2 - w;
+        horizontal = focus_dist * viewport_width * u;
+        vertical = focus_dist * viewport_height * v;
+        lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
+
+        lens_radius = aperture / 2;
     }
 
     [[nodiscard]] ray get_ray(double s, double t) const {
-        return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+
+        return ray(
+             origin + offset,
+             lower_left_corner + s*horizontal + t*vertical - origin - offset
+        );
     }
 
 private:
@@ -36,5 +46,7 @@ private:
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u, v, w;
+    double lens_radius;
 };
 #endif
