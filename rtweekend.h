bdb8c602251211f0738ba0f54f1139ecaeeffaf9
Mr-222 <821628267@qq.com>
2021-10-27 15:59:31 +0800

    fix a bug of random generator(rtweekend.h)

--- rtweekend.h
+++ rtweekend.h
@@ -21,19 +21,15 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-    // Returns a random real in [0, 1).
-    static std::random_device rd;
+    // Returns a random real in [0,1).
     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator(rd());
+    static std::mt19937 generator;
     return distribution(generator);
 }
 
 inline double random_double(double min, double max) {
-    // Returns a random rela in [min, max).
-    static std::random_device rd;
-    static std::uniform_real_distribution<double> distribution(min, max);
-    static std::mt19937 generator(rd());
-    return distribution(generator);
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 inline double clamp(double x, double min, double max) {
