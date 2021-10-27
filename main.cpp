97635d1724fd2e1579acc139dbf59a35afc60dfa
Mr-222 <821628267@qq.com>
2021-10-27 22:41:43 +0800

    fix a bug of random generator(rtweekend.h)

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
 int main() {
     // Image
-    constexpr double aspect_ratio = 3.0 / 2.0;
+    constexpr double aspect_ratio = 16.0 / 9.0;
     constexpr int image_width = 1200;
     constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
     constexpr int samples_per_pixel = 400;
--- rtweekend.h
+++ rtweekend.h
@@ -20,24 +20,16 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-inline double random_double() {
-    // Returns a random real in [0,1).
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    return distribution(generator);
-}
-
-inline double random_double(double min, double max) {
+inline double random_double(double min=0.0, double max=1.0) {
     // Returns a random real in [min,max).
-    return min + (max-min)*random_double();
+    static std::once_flag flag;
+    static std::mt19937 generator;
+    std::call_once(flag ,[&]() {
+        generator.seed(std::random_device{}());
+    });
+    static std::uniform_real_distribution<double> dis(min, max);
+    return dis(generator);
 }
-
-//inline double random_double(double min, double max) {
-//    // Returns a random real in [min,max).
-//    static std::uniform_real_distribution<double> distribution(min, max);
-//    static std::mt19937 generator;
-//    return distribution(generator);
-//}
 
 inline double clamp(double x, double min, double max) {
     if (x < min) return min;
