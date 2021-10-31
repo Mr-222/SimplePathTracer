54dfaf94e3ca7906adc918fbb616b5870692c815
Mr-222 <821628267@qq.com>
2021-10-31 14:18:53 +0800

    introduce perlin noise

--- perlin.h
+++ perlin.h
@@ -25,11 +25,25 @@ public:
     }
 
     [[nodiscard]] double noise(const point3& p) const {
-        int i = static_cast<int>(4*p.x()) & 255;
-        int j = static_cast<int>(4*p.y()) & 255;
-        int k = static_cast<int>(4*p.z()) & 255;
+        double u = p.x() - floor(p.x());
+        double v = p.y() - floor(p.y());
+        double w = p.z() - floor(p.z());
 
-        return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        int i = static_cast<int>(floor(p.x()));
+        int j = static_cast<int>(floor(p.y()));
+        int k = static_cast<int>(floor(p.z()));
+        double c[2][2][2];
+
+        for (int di=0; di<2; di++)
+            for (int dj=0; dj<2; dj++)
+                for (int dk=0; dk<2; dk++)
+                    c[di][dj][dk] = ranfloat[
+                            perm_x[(i+di) & 255] ^
+                            perm_y[(j+dj) & 255] ^
+                            perm_z[(k+dk) & 255]
+                    ];
+
+        return trilinear_interp(c, u, v, w);
     }
 
 private:
@@ -58,6 +72,18 @@ private:
             int target = random_int(0, i);
             std::swap(p[i], p[target]);
         }
+    }
+
+    static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+        double accum = 0.0;
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 2; ++j)
+                for (int k = 0; k < 2; ++k)
+                    accum += (i*u + (1-i)*(1-u))*
+                            (j*v + (1-j)*(1-v))*
+                            (k*w + (1-k)*(1-w)) * c[i][j][k];
+
+        return accum;
     }
 };
 
