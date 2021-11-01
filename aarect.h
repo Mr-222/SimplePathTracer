178c0f57241078c6b39f41f5cb3f476d58fdc300
Mr-222 <821628267@qq.com>
2021-11-01 22:02:23 +0800

    add scene "Cornell Box"

--- aarect.h
+++ aarect.h
@@ -88,7 +88,7 @@ public:
 
     yz_rect(double _y0, double _y1, double _z0, double _z1, double _k,
             shared_ptr<material> mat)
-            : y0(_y0), y1(_y1), z0(_z0), z1(_z1), k(_k), mp(std::(mat)) {};
+            : y0(_y0), y1(_y1), z0(_z0), z1(_z1), k(_k), mp(std::move(mat)) {};
 
     bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
