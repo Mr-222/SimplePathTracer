649c5d46e369c82ce36c0a841d7d544f5be72fb7
Mr-222 <821628267@qq.com>
2021-11-01 22:04:25 +0800

    add scene "Cornell Box"

--- aarect.h
+++ aarect.h
@@ -67,15 +67,17 @@ public:
 };
 
 bool xz_rect::hit(const ray &r, double t_min, double t_max, hit_record &rec) const {
-    double t = (k-r.origin().y()) / r.direction().y();
-    return !(t < t_min || t > t_max);
-    double x = r.origin().x() + t*r.direction().x();
-    double z = r.origin().z() + t*r.direction().z();
-    return !(x<x0 || x>x1 || z<z0 || z>z1);
-    rec.u = (x-x0) / (x1-x0);
-    rec.v = (z-z0) / (z1-z0);
+    auto t = (k-r.origin().y()) / r.direction().y();
+    if (t < t_min || t > t_max)
+        return false;
+    auto x = r.origin().x() + t*r.direction().x();
+    auto z = r.origin().z() + t*r.direction().z();
+    if (x < x0 || x > x1 || z < z0 || z > z1)
+        return false;
+    rec.u = (x-x0)/(x1-x0);
+    rec.v = (z-z0)/(z1-z0);
     rec.t = t;
-    vec3 outward_normal = vec3(0, 1, 0);
+    auto outward_normal = vec3(0, 1, 0);
     rec.set_face_normal(r, outward_normal);
     rec.mat_ptr = mp;
     rec.p = r.at(t);
@@ -106,10 +108,12 @@ public:
 
 bool yz_rect::hit(const ray &r, double t_min, double t_max, hit_record &rec) const {
     auto t = (k-r.origin().x()) / r.direction().x();
-    return !(t < t_min || t > t_max);
+    if (t < t_min || t > t_max)
+        return false;
     auto y = r.origin().y() + t*r.direction().y();
     auto z = r.origin().z() + t*r.direction().z();
-    return !(y < y0 || y > y1 || z < z0 || z > z1);
+    if (y < y0 || y > y1 || z < z0 || z > z1)
+        return false;
     rec.u = (y-y0)/(y1-y0);
     rec.v = (z-z0)/(z1-z0);
     rec.t = t;
